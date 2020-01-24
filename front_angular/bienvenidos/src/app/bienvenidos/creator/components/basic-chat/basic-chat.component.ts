0085ccd7935555eddb778aa7075365ec99590d7d
wilson.calixto <wilson.calixto@grupoicts.com.br>
2020-01-24 17:30:58 -0400

    adding change contacts feature

--- front_angular/bienvenidos/src/app/bienvenidos/creator/components/basic-chat/basic-chat.component.ts
+++ front_angular/bienvenidos/src/app/bienvenidos/creator/components/basic-chat/basic-chat.component.ts
@@ -1,25 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from 'src/app/bienvenidos/services/chat/message.service';
+import { ChatService } from 'src/app/bienvenidos/services/chat/chat.service';
+import { Type } from '@angular/compiler';
 @Component({
   selector: 'app-basic-chat',
   templateUrl: './basic-chat.component.html',
   styleUrls: ['./basic-chat.component.css']
 })
 export class BasicChatComponent implements OnInit {
+  contacts=[];
+  current_conversation;
+  id_do_usuario=8;
 
   constructor(
-    private MessageService: MessageService,                            
+    private messageService: MessageService,
+    private chatService: ChatService
+                           
     ) { }
+  
 
   ngOnInit() {
     
     this.subscribe_on_socket({macAddress:'sendMessage'});
+    this.findContacts('recents')
+
+  }
+  
+  findContacts(type_of_contacts){
+    console.log('@@@@@@@@@',type_of_contacts)
+    this.contacts=this.chatService.getContacts(1,type_of_contacts);
+    console.log('!!!!!!!!!!!!!',this.current_conversation)
   }
 
+  findConversation(contact){
 
+    this.current_conversation=this.chatService.getConversation(contact.id,this.id_do_usuario);
+    console.log('!!!!!!!!!!!!!',this.current_conversation)
+
+  }
   subscribe_on_socket(selectedPC){
     const mac_address=String(selectedPC.macAddress);
-    this.MessageService.getMessages(mac_address).subscribe((message: any) => {
+    this.messageService.getMessages(mac_address).subscribe((message: any) => {
       console.log('!!!!!!!!!!!!!',message)
     });
   }
@@ -27,7 +48,7 @@ export class BasicChatComponent implements OnInit {
 
   unsubscribe_on_socket(selectedPC){
     const mac_address=String(selectedPC.macAddress);
-    this.MessageService.unsubscribe(mac_address);
+    this.messageService.unsubscribe(mac_address);
   }
 
 }
--- front_angular/bienvenidos/src/app/bienvenidos/services/chat/chat.service.ts
+++ front_angular/bienvenidos/src/app/bienvenidos/services/chat/chat.service.ts
@@ -11,8 +11,26 @@ export class ChatService {
     
     constructor(private httpClient: HttpClient){}
 
-    getContats(id_do_usuario){
-        return this.httpClient.get(`${environment.API_URL}:${environment.API_PORT}/contatos/${id_do_usuario}`).toPromise();
+    getContacts(id_do_usuario,type){
+
+            // mockup "fake data"
+        if(type==='all'){
+            return [
+                {name:'Celina Passos',id_do_usuario:3},
+                {name:'Sebastian Gimenez',id_do_usuario:2},
+                {name:'Carlos Fernando',id_do_usuario:1}]
+        
+        }else{
+            return [
+                {name:'Sebastian Gimenez',id_do_usuario:2}]
+        }    
+
+        const obj={
+            'id_do_usuario':id_do_usuario,
+            'type':type
+        }
+             
+        // return this.httpClient.get(`${environment.API_URL}:${environment.API_PORT}/contatos/${obj}`).toPromise();
     }
 
     getConversation(id_do_emissor,id_do_receptor){
@@ -20,7 +38,16 @@ export class ChatService {
             'id_do_emissor':id_do_emissor,
             'id_do_receptor':id_do_receptor
         }
-        return this.httpClient.get(`${environment.API_URL}:${environment.API_PORT}/conversa/${obj}`).toPromise();
+                    // mockup "fake data"
+        return [
+        {message:'ola',timestamp:'Wed 01-01-2009 6:00','id_do_emissor':5,'id_do_receptor':2},
+        {message:'tudo bem ',timestamp:'Wed 01-01-2009 6:00','id_do_emissor':2,'id_do_receptor':5},
+        {message:'tudo sim, queria uma ajuda',timestamp:'Wed 01-01-2009 6:00','id_do_emissor':5,'id_do_receptor':2},
+        {message:'pode falar ',timestamp:'Wed 01-01-2009 6:00','id_do_emissor':2,'id_do_receptor':5}
+        ]
+        
+
+        // return this.httpClient.get(`${environment.API_URL}:${environment.API_PORT}/conversa/${obj}`).toPromise();
     }
 
 }
